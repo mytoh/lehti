7180781f20edb8b269f028c8fa0e509465e24135
mytoh <mytoh@gmail.com>
2014-03-07 02:54:21 +0900

    * lib/lehti/cli.scm: use r7rs

--- lib/lehti/cli.scm
+++ lib/lehti/cli.scm
@@ -1,9 +1,9 @@
 
-(define-module lehti.cli
-
-  (export run)
-  (use util.match)
-  (use lehti.command)
+(define-library (lehti cli)
+    (export run)
+  (import (scheme base)
+          (util match)
+          (lehti command))
 
   (begin
 
@@ -11,27 +11,27 @@
       (let ((command (cadr args))
             (rest (cddr args)))
         (match command
-          ("install"
-           (install rest))
-          ("clean"
-           (clean rest))
-          ("spec"
-           (specification rest))
-          ("list"
-           (list-packages rest))
-          ("env"
-           (environment rest))
-          ("setup"
-           (setup rest))
-          ("search"
-           (search rest))
-          ("deinstall"
-           (deinstall rest))
-          ("commands"
-           (commands rest))
-          ("bundle"
-           (bundle rest))
-          ("projects"
-           (projects rest)))))
+               ("install"
+                (install rest))
+               ("clean"
+                (clean rest))
+               ("spec"
+                (specification rest))
+               ("list"
+                (list-packages rest))
+               ("env"
+                (environment rest))
+               ("setup"
+                (setup rest))
+               ("search"
+                (search rest))
+               ("deinstall"
+                (deinstall rest))
+               ("commands"
+                (commands rest))
+               ("bundle"
+                (bundle rest))
+               ("projects"
+                (projects rest)))))
 
     ))
