f6d225cd9a56a0cb52742fbbacf38e72f64dc1cd
mytoh <mytoh@gmail.com>
2014-07-24 11:24:19 +0900

    * lib/lehti/command/specification.scm: use srfi-29's format

--- lib/lehti/command/specification.scm
+++ lib/lehti/command/specification.scm
@@ -4,8 +4,10 @@
       specification)
   ;; ** import
   (import (scheme base)
+          (scheme write)
           (gauche)
           (srfi 13)
+          (srfi 29)
           (lehti lehspec)
           (lehti env)
           (lehti base))
@@ -16,16 +18,17 @@
     (define (specification args)
       (let* ((package (car args))
              (lehspec (package->lehspec package)))
-        (format #true "~a ~a\n~a ~a\n~a\n~a\n"
-                (paint "name:" 39)
-                (spec.name lehspec)
-                (paint "description:" 29)
-                (spec.description lehspec)
-                (paint "files:" 66)
-                (string-join
-                    (map (lambda (s)
-                           (string-append "  - " s))
-                      (spec.files lehspec))
-                  "\n"))))
+        (display
+            (format "~a ~a\n~a ~a\n~a\n~a\n"
+              (paint "name:" 39)
+              (spec.name lehspec)
+              (paint "description:" 29)
+              (spec.description lehspec)
+              (paint "files:" 66)
+              (string-join
+                  (map (lambda (s)
+                         (string-append "  - " s))
+                    (spec.files lehspec))
+                "\n")))))
 
     ))
